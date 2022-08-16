b310413434f5dbdd5bc509a8a83b5766b0a49eb4
Lelabeshidze <lela.beshidze@gmail.com>
2022-08-17 01:43:29 +0400

    class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,111 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from "react";
+const generateUsers = () => {
+	return [
+		{
+			name: "Lela",
+			age: 23,
+			id: 1,
+		},
+		{
+			name: "Nino",
+			age: 20,
+			id: 2,
+		},
+		{
+			name: "Nika",
+			age: 43,
+			id: 3,
+		},
+		{
+			name: "Sandro",
+			age: 33,
+			id: 4,
+		},
+		{
+			name: "Elene",
+			age: 25,
+			id: 5,
+		},
+		{
+			name: "Salome",
+			age: 28,
+			id: 6,
+		},
+		{
+			name: "Beka",
+			age: 29,
+			id: 7,
+		},
+		{
+			name: "Gio",
+			age: 17,
+			id: 8,
+		},
+		{
+			name: "Teo",
+			age: 26,
+			id: 9,
+		},
+		{
+			name: "Saba",
+			age: 24,
+			id: 10,
+		},
+	];
+};
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
+	constructor() {
+		super();
+		this.state = { users: [], showUsersList: true };
+	}
+	componentDidMount() {
+		this.setState({ showUsersList: true });
+		const data = generateUsers();
+		this.setState({ users: data });
+	}
+	onClickHandler = () => {
+		this.setState((prevState) => {
+			const ind = Math.floor(Math.random() * prevState.users.length);
+			const newArray = prevState.users.filter((user, index) => index !== ind);
+			return { users: newArray };
+		});
+	};
+	changeOnClick = () => {
+		this.setState({ showUsersList: !this.state.showUsersList });
+	};
+	componentDidUpdate() {
+		if (this.state.users.length > 0) {
+			document.title = `${this.state.users.length}`;
+		} else {
+			document.title = "Unmounting";
+		}
+	}
+	// componentWillUnmount(){
+	// 	if (this.state.showUsersList = true) {
+	// 		<App/>
+	// 	} else {
+	// 		<h1>componentWillUnmount</h1>
+	// 	}
+	// }
+
+	render() {
+		const { users, showUsersList } = this.state;
+		console.log(this.state.users);
+		return (
+			<div>
+				{users.map((user) => {
+					const { name, age, id } = user;
+					return (
+						<div key={id}>
+							{name}
+							{age}
+						</div>
+					);
+				})}
+				<button onClick={this.onClickHandler}>Delete User</button>
+				<button onClick={this.changeOnClick}>Show list</button>
+			</div>
+		);
+	}
 }
